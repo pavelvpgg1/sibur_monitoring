7d4879e53f25a5fdb14d3042405613e283499f99
pavelvpgg1 <bogodpav@gmail.com>
2024-10-22 09:29:08 +0300

    version 1.0 (test)

---
+++ static/script.js
@@ -0,0 +1,10 @@
+function foo(){
+  let first = 5;
+  let second = document.getElementById('inputnumber').value;
+  if(first == second){
+    alert(document.getElementById('inputnumber').value);
+  }
+  else{
+    alert(document.getElementById('inputnumber').value);
+  }
+}
